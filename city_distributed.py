ce849e5340b989a276d8e6d45fd079c4afc787d6
RanLingsheng <ranlingsheng@021.com>
2021-11-22 09:32:56 +0800

    势力范围内的城池分布

---
+++ city_distributed.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2021/11/19 16:40 
+
+@author: R.ls
+"""
+
+from settings import *
+import zoom_out
+import result_view
+import valide_within
+import numpy as np
+import math
+import time
+import random
+import json
+
+
+def deal_path(sd, d):
+    zoom_dict = {}
+    find_dict = {}
+    n = 0
+    for point in turning_point:
+        zoom_array = list()
+        boundary = turning_point.get(point)
+
+        try:
+            zoom_x, zoom_y = zoom_out.sample(boundary, sd)
+            for i in range(len(zoom_x)):
+                zoom_array.append((zoom_x[i], zoom_y[i]))
+            # 安全范围内的字典数据
+            zoom_dict[point] = zoom_array
+
+            # 求出终点坐标 e_x = d * cos; e_y = d * sin
+            r = 60
+            find_cnt, find_array = find_six(point, r, zoom_array, d, boundary)
+
+            while len(find_array) < 6 and r > 0:
+                r = r - 5
+                # print(r)
+                find_cnt, find_array = find_six(point, r, zoom_array, d, boundary)
+
+            if len(find_array) != 6:
+                print(r, len(find_array))
+                # print(zoom_array)
+
+            find_dict[str(point)] = find_array
+
+            if len(boundary[0])>4:
+                print(n)
+                result_view.sample(boundary, sd, find_array, point, n)
+
+            n += 1
+
+            # print(find_dict)
+        except Exception as e:
+            print(point)
+    return find_dict
+
+
+def find_six(point, r, zoom_array, d, boundary):
+    find_array = list()
+    re = 0
+    find_cnt = 0
+    max = int(360 / r)
+    for seg in range(1, max):
+        if find_cnt >= 6:
+            continue
+        e_x = round(point[0] + d * math.cos(math.radians(re)), 1)
+        e_y = round(point[1] + d * math.sin(math.radians(re)), 1)
+        re += r
+        # 是否在势力范围内
+        if valide_within.isPoiWithinPoly((e_x, e_y), [zoom_array]) :
+            find_array.append((e_x, e_y))
+            find_cnt += 1
+    return find_cnt, find_array
+
+
+if __name__ == '__main__':
+    sd = 5
+    d = 12.5
+    find_dict = deal_path(sd, d)
+    # print(len(find_dict))
+    result_dict={}
+    tmp = list()
+    with open("C:/Users/admin/Desktop/city_distribute.txt", "w") as fw:
+        # fw.write(str(json.dumps(find_dict)))
+        fw.write(str(find_dict))
---
+++ test.py
@@ -0,0 +1,121 @@
+from matplotlib import pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+def Pic(data):
+    x = []
+    for i in range(len(data)):
+        x.append(data[i][0])
+    y = []
+    for i in range(len(data)):
+        y.append(data[i][1])
+    return x, y
+
+def sample(data, sd):
+    for i in range(len(data)):
+        x, y = Pic(data[i])
+        x.append(x[0])  # 增加第一个坐标使得矩形封闭
+        y.append(y[0])
+
+        plt.plot(x, y, c='r')
+
+    x.append(x[1])
+
+    y.append(y[1])
+
+
+    # 求两条邻边的向量
+    new_data = []
+    Qi_x_list = []
+    Qi_y_list = []
+    for i in range(len(x)):
+        if i < (len(x) - 2):
+            # print("=========")
+            # print(x[i],y[i])
+            # print(x[i+1], y[i+1])
+            # 求边的长度
+            d1 = ((x[i + 1] - x[i]) ** 2 + (y[i + 1] - y[i]) ** 2) ** 0.5
+            d2 = ((x[i + 1] - x[i + 2]) ** 2 + (y[i + 1] - y[i + 2]) ** 2) ** 0.5
+            # 两条边的夹角
+            # 内积ab,sinA
+            # ab=(x[i+1]-x[i])*(y[i+1]-y[i])+(x[i+1]-x[i+2])*(y[i+1]-y[i+2])
+            ab = (x[i + 1] - x[i]) * (x[i + 1] - x[i + 2]) + (y[i + 1] - y[i]) * (y[i + 1] - y[i + 2])
+            cosA = ab / (d1 * d2)
+
+            if cosA >= 0:
+
+                # x[]
+                # 判断凹凸点（叉积）
+                P1P3_x = x[i + 2] - x[i]
+                P1P3_y = y[i + 2] - y[i]
+                P1P2_x = x[i + 1] - x[i]
+                P1P2_y = y[i + 1] - y[i]
+                # P=P1P3 x P1P2
+                P = (P1P3_y * P1P2_x) - (P1P3_x * P1P2_y)
+                # 为凹时
+                if P > 0:
+                    sinA = -((1 - cosA ** 2) ** 0.5)
+
+                    # 向量V1,V2的坐标
+                    dv1 = sd / sinA  # V1,V2长度相等
+                    v1_x = (dv1 / d1) * (x[i + 1] - x[i])
+                    v1_y = (dv1 / d1) * (y[i + 1] - y[i])
+                    v2_x = (dv1 / d2) * (x[i + 1] - x[i + 2])
+                    v2_y = (dv1 / d2) * (y[i + 1] - y[i + 2])
+                    PiQi_x = v1_x + v2_x
+                    PiQi_y = v1_y + v2_y
+                    Qi_x = PiQi_x + x[i + 1]
+                    Qi_x_list.append(Qi_x)
+                    Qi_y = PiQi_y + y[i + 1]
+                    Qi_y_list.append(Qi_y)
+                elif P < 0:
+                    sinA = -((1 - cosA ** 2) ** 0.5)
+
+                    # 向量V1,V2的坐标
+                    dv1 = -sd / sinA  # V1,V2长度相等
+                    v1_x = (dv1 / d1) * (x[i + 1] - x[i])
+                    v1_y = (dv1 / d1) * (y[i + 1] - y[i])
+                    v2_x = (dv1 / d2) * (x[i + 1] - x[i + 2])
+                    v2_y = (dv1 / d2) * (y[i + 1] - y[i + 2])
+                    PiQi_x = v1_x + v2_x
+                    PiQi_y = v1_y + v2_y
+                    Qi_x = PiQi_x + x[i + 1]
+                    Qi_x_list.append(Qi_x)
+                    Qi_y = PiQi_y + y[i + 1]
+                    Qi_y_list.append(Qi_y)
+
+            elif cosA < 0:
+
+                sinA = (1 - cosA ** 2) ** 0.5
+
+                # 向量V1,V2的坐标
+                dv1 = sd / sinA  # V1,V2长度相等
+                v1_x = (dv1 / d1) * (x[i + 1] - x[i])
+                v1_y = (dv1 / d1) * (y[i + 1] - y[i])
+                v2_x = (dv1 / d2) * (x[i + 1] - x[i + 2])
+                v2_y = (dv1 / d2) * (y[i + 1] - y[i + 2])
+                PiQi_x = v1_x + v2_x
+                PiQi_y = v1_y + v2_y
+                Qi_x = PiQi_x + x[i + 1]
+                Qi_x_list.append(Qi_x)
+                Qi_y = PiQi_y + y[i + 1]
+                Qi_y_list.append(Qi_y)
+                # new_data.append(zip(Qi_x, Qi_y))
+
+
+    Qi_x_list.append(Qi_x_list[0])
+    Qi_y_list.append(Qi_y_list[0])
+    print(Qi_x_list)
+    print(Qi_y_list)
+    plt.plot(Qi_x_list, Qi_y_list, c='g')
+    plt.show()
+
+
+if __name__ == '__main__':
+    data=[[(983.5,1751.5),(952.5,1751.5),(952.5,1698.5),(970.5,1698.5),(970.5,1692.0),(983.5,1692.0)]]
+    # data = [[(10, 10), (10, 20), (15, 20), (15, 17), (30, 15), (25, 0), (20, 12)]]
+    sd = 5  # 安全距离
+    sample(data, sd)
+
+
+
